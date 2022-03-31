deb770faae30f17702409085c04bcfc74f508c6a
rasa mikuleviciene <rasamikuleviciene@Air-rasa.lan>
2022-03-31 10:48:42 +0300

    Sukurtas projektas

---
+++ JS-1 UZDUOTIS/JS script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    let x;
+    let y;
+    let z;
+
+    let suma;
+    x = 50;
+    y = 20;
+    z = -100;
+    suma = x + y + z;
+    console.log(suma);
+    
+    
+}
